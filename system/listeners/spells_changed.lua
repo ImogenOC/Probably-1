8e26a37a10df2fd4c7c263c98395dff2f80d82f2
Ben Phelps <ben@phelps.io>
2014-10-31 00:42:52 -0500

    I don't even know what this did...

--- system/listeners/spells_changed.lua
+++ system/listeners/spells_changed.lua
@@ -1,4 +1,4 @@
 -- ProbablyEngine Rotations - https://probablyengine.com/
 -- Released under modified BSD, see attached LICENSE.
 
-ProbablyEngine.listener.register('SPELLS_CHANGED', clearOverloadCache)
+--ProbablyEngine.listener.register('SPELLS_CHANGED', clearOverloadCache)
