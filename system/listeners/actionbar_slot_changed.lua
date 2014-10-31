33cd52869a13575b6d365dd4645ad7b74913e756
Ben Phelps <ben@phelps.io>
2014-10-31 00:43:31 -0500

    there are 12 buttons, not 10

--- system/listeners/actionbar_slot_changed.lua
+++ system/listeners/actionbar_slot_changed.lua
@@ -11,7 +11,7 @@ ProbablyEngine.listener.register('ACTIONBAR_SLOT_CHANGED', function ()
 		"MultiBarLeftButton"
 	}
 	for _, group in ipairs(bars) do
-		for i =1, 10 do
+		for i =1, 12 do
 			local button = _G[group .. i]
 			if button then
 				local actionType, id, subType = GetActionInfo(ActionButton_CalculateAction(button, "LeftButton"))
